16e350dc76dace731476f43e316e6592e82d2a2f
andresrechimon <111940@tecnicatura.frc.utn.edu.ar>
2022-04-29 17:04:48 -0300

    diceApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'diceApp';
+  number1!:number;
+  number2!:number;
+  dice1:string = `../assets/img/dice2.png`;
+  dice2:string = `../assets/img/dice5.png`;
+
+  roll():void{
+    this.number1 = Math.round(Math.random() * 5) + 1;
+    this.number2 = Math.round(Math.random() * 5) + 1;
+    this.dice1 = '../assets/img/dice' + this.number1 + '.png';
+    this.dice2 = '../assets/img/dice' + this.number2 + '.png';
+  }
 }
